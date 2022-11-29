90c9aba1762922ad1fa7c0d96ac07977a69a2ddc
conshipl <conshipl@tesla.cs.iupui.edu>
2022-11-29 13:16:39 +0000

    Commit (I Think) Final, Finished Version Of Project (Unless I Can Find Something Else Bothering Me)

--- java/MainMenu.java
+++ java/MainMenu.java
@@ -14,8 +14,6 @@ public class MainMenu extends ScreenHelper {
     this.menu();
   } // end default constructor
 
-  /* Should be able to visualize binary tree (print in console) per Jovian
-
   /* Should be able to search and view the stats for an individual player (W/D/L and Most/Least Played Monsters)*/
   /* You can deserialize the same object twice into two different (in memory location) instances with the same data*/
 
@@ -25,26 +23,53 @@ public class MainMenu extends ScreenHelper {
     while (keepGoing){
       Scanner input = new Scanner(System.in);
       System.out.println("---KING OF TOKYO---\n");
-      System.out.print("1) Start New Game\n2) View Player Tree\n3) Search Player Stats\n4) Exit\n\nEnter Choice: ");
+      System.out.print("1) Start New Game\n2) View All Players\n3) Search Player Stats\n4) Exit\n\nEnter Choice: ");
       String choice = input.nextLine();
 
       if (choice.equals("1")){
 	this.clearScreen();
         this.game = new Game();
       } else if (choice.equals("2")){
-        this.populateAccounts();
-	this.clearScreen();
-	for (Player account: this.accounts){
-          account.showStats();
-        } // end for
-	this.pressAnyKey();
+        this.viewAllPlayers();
       } else if (choice.equals("3")){
-        this.populateAccounts();
+        this.searchPlayer();
       } else if (choice.equals("4")){
         keepGoing = false;
+	this.clearScreen();
       } // end if
     } // end while
   } // end menu
+
+  public void searchPlayer(){
+    this.populateAccounts();
+    this.clearScreen();
+    boolean found = false;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter Player Name: ");
+    String target = input.nextLine();
+    for (Player account: this.accounts){
+      if (account.name.equals(target)){
+	System.out.println("");
+        account.showStats();
+	found = true;
+	System.out.println("");
+      } // end if
+    } // end for
+    if (!found){
+      System.out.println("\nPlayer Not Found.\n");
+    } // end if
+    this.pressAnyKey();
+  } // end searchPlayer
+
+  public void viewAllPlayers(){
+    this.populateAccounts();
+    this.clearScreen();
+    for (Player account: this.accounts){
+      account.showStats();
+      System.out.println("");
+    } // end for
+    this.pressAnyKey();
+  } // end viewAllPlayers
 
   public void populateAccounts(){
     try {
